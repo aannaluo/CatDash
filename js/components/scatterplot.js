bc7e9dbfb878b7917cdfa64aca1da26b26ff4aa6
helenjym <helenjym@gmail.com>
2026-03-09 16:21:47 -0700

    add legend

--- js/components/scatterplot.js
+++ js/components/scatterplot.js
@@ -1,5 +1,27 @@
 // window.renderScatterplot = () => '<h2>Hello</h2><script>new Timeline().renderVis() </script>';
 
+function linearRegression(y, x) {
+  const lr = {};
+  const n = y.length;
+  let sumX = 0;
+  let sumY = 0;
+  let sumXY = 0;
+  let sumXX = 0;
+  let sumYY = 0;
+
+  for (let i = 0; i < y.length; i++) {
+    sumX += x[i];
+    sumY += y[i];
+    sumXY += (x[i] * y[i]);
+    sumXX += (x[i] * x[i]);
+    sumYY += (y[i] * y[i]);
+  }
+  lr.slope = (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX);
+  lr.intercept = (sumY - lr.slope * sumX) / n;
+  lr.r2 = ((n * sumXY - sumX * sumY) / Math.sqrt((n * sumXX - sumX * sumX) * (n * sumYY - sumY * sumY))) ** 2;
+
+  return lr;
+}
 // eslint-disable-next-line no-unused-vars
 class Scatterplot {
   /**
@@ -11,10 +33,10 @@ class Scatterplot {
     this.config = {
       parentElement: _config.parentElement,
       containerWidth: 650,
-      containerHeight: 310,
+      containerHeight: 250,
       tooltipPadding: 15,
       margin: {
-        top: 80, right: 20, bottom: 20, left: 45,
+        top: 10, right: 20, bottom: 20, left: 45,
       },
       legendWidth: 170,
       legendHeight: 8,
@@ -48,6 +70,19 @@ class Scatterplot {
     vis.xAxisG = vis.chartArea.append('g');
     vis.yAxisG = vis.chartArea.append('g');
 
+    vis.svg.append('text')
+    .attr('transform', `translate(${(vis.width + 50) / 2}, ${vis.height + vis.config.margin.top + 17})`)
+  //   .attr('x', vis.width / 2)
+  //   .attr('y', vis.height + vis.config.margin.top)
+    .text('Age (years)')
+    .attr('font-size', '0.8em');
+
+    vis.svg.append('text')
+    .attr('transform', 'rotate(-90) translate(-170, 50)')
+  //   .attr('x', vis.width / 2)
+  //   .attr('y', vis.height + vis.config.margin.top)
+    .text('Home Range (km^2)')
+    .attr('font-size', '0.8em');
     vis.updateVis();
   }
 
@@ -58,14 +93,18 @@ class Scatterplot {
     const vis = this;
     const maxAge = d3.max(vis.data, (d) => d.Age);
     const maxHR = d3.max(vis.data, (d) => d['Home Range Area (km2)']);
+    const lr = linearRegression(vis.data.map((d) => d['Home Range Area (km2)']), vis.data.map((d) => d.Age));
+    console.log(lr);
+    vis.lineStartY = lr.intercept;
+    vis.lineEndY = (lr.slope * maxAge) + lr.intercept;
+    console.log(vis.lineStartY);
+    console.log(vis.lineEndY);
     vis.xScale
       .domain([0, maxAge]);
     vis.yScale
       .domain([maxHR, 0]);
     vis.xAxis = d3.axisBottom(vis.xScale);
     vis.yAxis = d3.axisLeft(vis.yScale);
-    vis.xAxisG = vis.chartArea.append('g');
-    vis.yAxisG = vis.chartArea.append('g');
     vis.renderVis();
   }
 
@@ -86,62 +125,50 @@ class Scatterplot {
       .attr('transform', `translate(0, ${vis.height - 10})`);
 
     const catGroupNeutered = vis.chartArea.selectAll('.splot-point')
-      .data(vis.data.filter((d) => d.Neutered === 'Yes'), (d) => d[0]);
+      .data(vis.data.filter((d) => d.Neutered === 'Yes'), (d) => (d.ID + d['Study Name']));
 
     const catGroupNeuteredAdded = catGroupNeutered.enter()
       .append('circle')
       .attr('class', 'splot-point')
       .attr('r', 4)
+      .attr('stroke', 'black')
+      .attr('stroke-width', 0.3)
+      .attr('opacity', 0.9);
+
+    catGroupNeuteredAdded.merge(catGroupNeutered)
+      .attr('cx', (d) => `${vis.xScale(d.Age)}`)
+      .attr('cy', (d) => `${vis.yScale(d['Home Range Area (km2)'])}`)
       .attr('fill', (d) => {
         if (d.Sex === 'Female') {
           return '#ff88c4';
         }
         return '#0095da';
-      })
-      .attr('stroke', 'black')
-      .attr('stroke-width', 0.3);
-
-    catGroupNeuteredAdded.merge(catGroupNeutered)
-      .attr('cx', (d) => `${vis.xScale(d.Age)}`)
-      .attr('cy', (d) => `${vis.yScale(d['Home Range Area (km2)'])}`);
+      });
 
     catGroupNeutered.exit().remove();
 
     const catGroupNotNeutered = vis.chartArea.selectAll('.splot-point-square')
-      .data(vis.data.filter((d) => d.Neutered === 'No'), (d) => d[0]);
+      .data(vis.data.filter((d) => d.Neutered === 'No'), (d) => (d.ID + d['Study Name']));
 
     const catGroupNotNeuteredAdded = catGroupNotNeutered.enter()
       .append('rect')
       .attr('class', 'splot-point-square')
       .attr('width', 8)
       .attr('height', 8)
-      .attr('fill', (d) => {
-        if (d.Sex === 'Female') {
-          return '#ff88c4';
-        }
-        return '#0095da';
-      })
+
       .attr('stroke', 'black')
       .attr('stroke-width', 0.3);
 
     catGroupNotNeuteredAdded.merge(catGroupNotNeutered)
       .attr('x', (d) => `${vis.xScale(d.Age)}`)
-      .attr('y', (d) => `${vis.yScale(d['Home Range Area (km2)'])}`);
+      .attr('y', (d) => `${vis.yScale(d['Home Range Area (km2)'])}`)
+      .attr('fill', (d) => {
+        if (d.Sex === 'Female') {
+          return '#ff88c4';
+        }
+        return '#0095da';
+      });
 
     catGroupNotNeutered.exit().remove();
-
-    vis.svg.append('text')
-      .attr('transform', `translate(${(vis.width + 50) / 2}, ${vis.height + vis.config.margin.top + 17})`)
-    //   .attr('x', vis.width / 2)
-    //   .attr('y', vis.height + vis.config.margin.top)
-      .text('Age (years)')
-      .attr('font-size', '0.8em');
-
-    vis.svg.append('text')
-      .attr('transform', 'rotate(-90) translate(-230, 50)')
-    //   .attr('x', vis.width / 2)
-    //   .attr('y', vis.height + vis.config.margin.top)
-      .text('Home Range (km^2)')
-      .attr('font-size', '0.8em');
-  }
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,17 +1,42 @@
 d3.csv('./data/Cleaned Cat Data.csv').then((data) => {
   // eslint-disable-next-line no-unused-vars, no-undef
+  data.forEach((d) => {
+    d.Age = parseFloat(d.Age);
+    d['Home Range Area (km2)'] = parseFloat(d['Home Range Area (km2)']);
+  });
   const splot = new Scatterplot({
-    parentElement: '#scatterplot .vis',
+    parentElement: '#scatterplot .vis'
   }, data);
+  let chosenCategories = [];
+  d3.selectAll('.radio-button-group').on(
+    'click',
+    (event) => {
+      const clickedCategory = event.currentTarget.classList[1];
+      if (chosenCategories.includes(clickedCategory)) {
+        chosenCategories.splice(chosenCategories.indexOf(clickedCategory), 1);
+        d3.select(`.radio-button-label.${clickedCategory}`)
+          .classed('active', false);
+      } else {
+        chosenCategories.push(clickedCategory);
+        d3.select(`.radio-button-label.${clickedCategory}`)
+          .classed('active', true);
+      }
+      if (chosenCategories.length !== 0) {
+        const filteredData = data.filter((d) => chosenCategories.includes(d.Sex));
+        splot.data = filteredData;
+      } else {
+        splot.data = data;
+      }
+      splot.updateVis();
+    }
+  );
 });
-
 d3.json('./data/cat_bubbles_all.json').then((data) => {
   // eslint-disable-next-line no-unused-vars, no-undef
   const bubbleMap = new BubbleMap({
     parentElement: '#bubbleMap',
   }, data);
 });
-
 d3.csv('./data/Distance_Travelled.csv').then((data) => {
   data.forEach((d) => {
     // eslint-disable-next-line no-param-reassign
