f50b484da46a3215a8b967d16bc2b005293111fd
helenjym <helenjym@gmail.com>
2026-03-08 19:21:14 -0700

    add date folder

--- js/main.js
+++ js/main.js
@@ -1,26 +1,3 @@
-// const Scatterplot = require('./components/scatterplot');
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const description = document.getElementById('description');
-//   const bubbleMap = document.getElementById('bubbleMap');
-//   const radialChart = document.getElementById('radialChart');
-//   const scatterplot = document.getElementById('scatterplot');
-//   if (description && window.renderDescription) {
-//     description.innerHTML = window.renderDescription();
-//     // load data, create a ne scatterplot and call initVis
-//   }
-//   if (bubbleMap && window.renderBubbleMap) {
-//     bubbleMap.innerHTML = window.renderBubbleMap();
-//   }
-
-//   if (radialChart && window.renderRadialChart) {
-//     radialChart.innerHTML = window.renderRadialChart();
-//   }
-
-//   if (scatterplot && window.renderScatterplot) {
-//     scatterplot.innerHTML = window.renderScatterplot();
-//   }
-// });
 d3.csv('Cleaned Cat Data.csv').then((data) => {
   // eslint-disable-next-line no-unused-vars, no-undef
   const splot = new Scatterplot({
