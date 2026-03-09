364fa4d33b8b09c880acafc179e5a7b5d5b278e4
helenjym <helenjym@gmail.com>
2026-03-08 19:26:57 -0700

    fix data reference

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-d3.csv('Cleaned Cat Data.csv').then((data) => {
+d3.csv('./data/Cleaned Cat Data.csv').then((data) => {
   // eslint-disable-next-line no-unused-vars, no-undef
   const splot = new Scatterplot({
     parentElement: '#scatterplot .vis',
